58e033d782c1773673404580d66b9b34055e4bd4
thiago_vieira2 <ra58029747888@acaonsfatima.org.br>
2025-04-18 04:44:43 -0300

    ss

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -5,20 +5,35 @@ export function Sobre() {
 
 
     return (
-        
+
 
 
 
         <div className="main-sobre">
             <div className="sobre-nos">
 
-               
+
 
                 <div className="text-sobre">
 
-                    <h1>
-                        Sobre nós
-                    </h1>
+                    <div className="tittle">
+                        <h1>
+                            Sobre nós
+                        </h1>
+
+
+                        <div className="columns-animation">
+                            <div className="columns1">
+
+                            </div>
+
+                            <div className="columns2">
+
+                            </div>
+                        </div>
+
+                    </div>
+
 
                     <div className="historia">
                         <h3>Historia</h3>
@@ -27,36 +42,41 @@ export function Sobre() {
                     </div>
                 </div>
 
-                <div className="columns-animation">
-                    <div className="columns1">
-                        
-                    </div>
 
-                    <div className="columns2">
-
-                    </div>
-                </div>
             </div>
+
+
             <div className="diferenciais">
-                <div className="columns-animation">
-                    <div className="columns1">
 
+                <div className="text-diferenciais">
+
+                    <div className="tittle-diferenciais">
+
+                        <h3>Difenciais</h3>
+                        <div className="columns-animation">
+                            <div className="columns1">
+
+                            </div>
+
+                            <div className="columns2">
+
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="columns2">
-
+                    <div className="texto">
+                        <ul className="lista-de-diferenciais">
+                            <li> Desenvolvimento ágil – Entregamos projetos com rapidez e eficiência</li>
+                            <li> Personalização total – Criamos soluções sob medida para cada cliente</li>
+                            <li>Tecnologia moderna – Utilizamos as melhores ferramentas do mercado</li>
+                            <li>Suporte próximo – Atendimento direto e acompanhamento do projeto</li>
+                        </ul>
                     </div>
                 </div>
-
-                <h3>Difencial</h3>
-
-                <ul className="diferenciais">
-                    <li> Desenvolvimento ágil – Entregamos projetos com rapidez e eficiência</li>
-                    <li> Personalização total – Criamos soluções sob medida para cada cliente</li>
-                    <li>Tecnologia moderna – Utilizamos as melhores ferramentas do mercado</li>
-                    <li>Suporte próximo – Atendimento direto e acompanhamento do projeto</li>
-                </ul>
             </div>
+            {/* 
+              */}
         </div>
+
     );
 }
