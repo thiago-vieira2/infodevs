9d3ce2b79e68cda5e8654176a4baf11b814f62f7
Cauã Andrade <caua.goldenboyy@gmail.com>
2025-04-22 21:20:06 -0300

    lógica email

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -1,6 +1,50 @@
+import emailjs from 'emailjs-com';
 import './index.scss';
+import { useState } from 'react';
+
 
 export function Contato () {
+
+        
+       const[emailCliente,setEmailCliente] = useState('') 
+       const[assunto, setAssunto] = useState('')
+       const[mensagemEmail, setMensagemEmail] = useState('')
+
+
+    function EnviarEmail(){
+        if(!emailCliente || !assunto || !mensagemEmail){   // eu que to documentando, validação se todos os inputs estão preenchidos
+            console.log('preencha todos os campos') 
+            alert("preencha todos os campos ") // se nao, aparece um window alert
+            return
+        }
+
+        // função do email js, que chama o método send, pra  fazer o ENVIO de um email
+        emailjs.send(
+            'service_jfyt0xr',         
+            'template_4ildg3d',         
+            {
+              email: emailCliente,   
+              subject: assunto,     
+              message: mensagemEmail
+                        
+            },
+            
+            'X7rGGiXe092ksd_LO'       
+        ).then(function(response) {
+            console.log('Email enviado com sucesso:', response.status, response.text);
+            
+            alert('Enviado com sucesso!');
+
+            setAssunto('')
+            setEmailCliente('')
+            setMensagemEmail('')
+        })
+            
+     
+    }
+   
+
+
     return (
         <div className="main-contato">
             <div className="tittle-contato">
@@ -17,11 +61,11 @@ export function Contato () {
             </div>
 
             <div className="conteiner-contato">
-                <input type="email" name="" id="" placeholder='Digite seu email' className='Email-user' />
-                <input type="text" className='Assunto' placeholder='Digite o assunto' />
-                <textarea className='mensagem' name="" id="" placeholder='Digite sua mensagem'></textarea>
+                <input value={emailCliente} onChange={(e) => setEmailCliente(e.target.value)} type="email" name="" id="" placeholder='Digite seu email' className='Email-user' />
+                <input value={assunto} onChange={(e) => setAssunto(e.target.value)} type="text" className='Assunto' placeholder='Digite o assunto' />
+                <textarea value={mensagemEmail} onChange={(e) => setMensagemEmail(e.target.value)} className='mensagem' name="" id="" placeholder='Digite sua mensagem'></textarea>
             </div>
-            <button className='send-email'>Enviar</button>
+            <button onClick={EnviarEmail} className='send-email'>Enviar</button>
         </div>
     );
 }
